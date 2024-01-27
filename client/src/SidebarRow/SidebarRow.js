c290cd74c0cab051ed16e195106366f811d69da7
wojtko <j.w2001@onet.pl>
2024-01-27 17:54:18 +0100

    Improve Sidebar

--- client/src/SidebarRow/SidebarRow.js
+++ client/src/SidebarRow/SidebarRow.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./SidebarRow.css";
 import { Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function SidebarRow({ data }) {
   return (
     <div className="sidebarRow">
-      {data ? <Avatar src={data.userImage} /> : <Avatar />}
-      <p className="userName">{data.name}</p>
+      <Link to={`/users/${data._id}`}>
+        {data ? <Avatar src={data.userImage} /> : <Avatar />}
+        <p className="userName">{data.name}</p>
+      </Link>
     </div>
   );
 }
