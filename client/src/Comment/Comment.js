3d2780c7fd1fa696b2bb132a189b19aa41c7417d
wojtko <j.w2001@onet.pl>
2024-01-28 22:08:39 +0100

    Display the current comments of a post

--- client/src/Comment/Comment.js
+++ client/src/Comment/Comment.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Avatar } from '@mui/material';
-import './Comment.css';
+import React from "react";
+import { Avatar } from "@mui/material";
+import "./Comment.css";
 
-function Comment() {
-	return (
-		<div className='comment'>
-			<div className='commentTop'>
-				<Avatar />
-				<div className='commentInfo'>
-					<h6>Adison Rakietson</h6>
-					<p>Treść komentarza</p>
-				</div>
-			</div>
-		</div>
-	);
+function Comment({ comment }) {
+  console.log(comment);
+  return (
+    <div className="comment">
+      <div className="commentTop">
+        <Avatar />
+        <div className="commentInfo">
+          <h6>{comment.author}</h6>
+          <p>{comment.body}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
--- client/src/Post/Post.js
+++ client/src/Post/Post.js
@@ -1,134 +1,145 @@
-import { React, useState } from 'react';
-import { Avatar } from '@mui/material';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-import CommentIcon from '@mui/icons-material/Comment';
-import Moment from 'react-moment';
-import './Post.css';
-import { Link } from 'react-router-dom';
+import { React, useEffect, useState } from "react";
+import { Avatar } from "@mui/material";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import CommentIcon from "@mui/icons-material/Comment";
+import Moment from "react-moment";
+import "./Post.css";
+import { Link } from "react-router-dom";
+import Comment from "../Comment/Comment";
 
 function Post({ data }) {
-	const [isLike, setIsLike] = useState(false);
-	const [isClickedComment, setIsClickedComment] = useState(false);
-	const [commentContent, setCommentContent] = useState('');
+  const [isLike, setIsLike] = useState(false);
+  const [isClickedComment, setIsClickedComment] = useState(false);
+  const [commentContent, setCommentContent] = useState("");
+  const [post, setPost] = useState([]);
 
-	const likeChange = async () => {
-		setIsLike(!isLike);
-		let updateLikes = parseInt(data.likes) + 1;
+  console.log(post);
 
-		if (isLike) {
-			await fetch(`/posts/update/${data._id}`, {
-				method: 'PATCH',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({
-					likes: updateLikes,
-				}),
-			})
-				.then(res => {
-					if (res.status === 200) {
-						console.log('ok');
-					}
-				})
-				.then(data => console.log(data))
-				.catch(error => console.error(error));
-		}
-	};
+  const likeChange = async () => {
+    setIsLike(!isLike);
+    let updateLikes = parseInt(data.likes) + 1;
 
-	const handleCommentClick = () => {
-		setIsClickedComment(!isClickedComment);
-	};
+    if (isLike) {
+      await fetch(`/posts/update/${data._id}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          likes: updateLikes,
+        }),
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            console.log("ok");
+          }
+        })
+        .then((data) => console.log(data))
+        .catch((error) => console.error(error));
+    }
+  };
 
-	const handleCommentChange = e => {
-		setCommentContent(e.target.value);
-	};
+  const handleCommentClick = () => {
+    setIsClickedComment(!isClickedComment);
 
-	const handleCommentSubmit = e => {
-		e.preventDefault();
-		handleCommentClick();
-	};
+    // POST request
+  };
 
-	return (
-		<div className='post'>
-			<div className='postTop'>
-				<Avatar className='postAvatar' src={data.userImage} />
-				<div className='info'>
-					<Link to={`/users/${data.userId}`}>
-						<h3>{data.author}</h3>
-					</Link>
-					<p>
-						<Moment format='DD/MM/YYYY'>{data.date}</Moment>
-					</p>
-				</div>
-			</div>
+  const handleCommentChange = (e) => {
+    setCommentContent(e.target.value);
+  };
 
-			<div className='postBottom'>
-				<p>{data.title}</p>
-			</div>
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    handleCommentClick();
+  };
 
-			<div className='postImg'>
-				<img src={data.postImg} alt='' width='100%' />
-			</div>
+  useEffect(() => {
+    setPost(data);
+  }, [data]);
 
-			<div className='postStats'>
-				<div className='likes'>
-					<ThumbUpIcon />
-					<p>
-						<span>{data.likes}</span>
-					</p>
-				</div>
+  return (
+    <div className="post">
+      <div className="postTop">
+        <Avatar className="postAvatar" src={data.userImage} />
+        <div className="info">
+          <Link to={`/users/${data.userId}`}>
+            <h3>{data.author}</h3>
+          </Link>
+          <p>
+            <Moment format="DD/MM/YYYY">{data.date}</Moment>
+          </p>
+        </div>
+      </div>
 
-				<div className='comments'>
-					<CommentIcon />
-					<p>
-						<span>{data.comments.length}</span> komentarzy
-					</p>
-				</div>
-			</div>
+      <div className="postBottom">
+        <p>{data.title}</p>
+      </div>
 
-			<div className='postOptions'>
-				<div className='postOpt' onClick={likeChange}>
-					{isLike ? (
-						<>
-							<ThumbUpIcon className='postClicked' />
-							<p>Polubiono</p>
-						</>
-					) : (
-						<>
-							<ThumbUpIcon />
-							<p>Polub</p>
-						</>
-					)}
-				</div>
+      <div className="postImg">
+        <img src={data.postImg} alt="" width="100%" />
+      </div>
 
-				<div className='postOpt' onClick={handleCommentClick}>
-					<CommentIcon />
-					<p>Dodaj komentarz</p>
-				</div>
-			</div>
+      <div className="postStats">
+        <div className="likes">
+          <ThumbUpIcon />
+          <p>
+            <span>{data.likes}</span>
+          </p>
+        </div>
 
-			<div className='postComms'>
-				{/* Wyświetlanie komentarzy */}
-				<Comment />
-				<Comment />
-				<Comment />
-			</div>
+        <div className="comments">
+          <CommentIcon />
+          <p>
+            <span>{data.comments.length}</span> komentarzy
+          </p>
+        </div>
+      </div>
 
-			{isClickedComment ? (
-				<form className='addComment' onSubmit={handleCommentSubmit}>
-					<input
-						type='text'
-						placeholder='Treść komentarza...'
-						className='addCommentInput'
-						onChange={handleCommentChange}
-						value={commentContent}
-					/>
-					<br />
-					<button type='submit' className='addCommentBtn'>
-						Dodaj
-					</button>
-				</form>
-			) : null}
-		</div>
-	);
+      <div className="postOptions">
+        <div className="postOpt" onClick={likeChange}>
+          {isLike ? (
+            <>
+              <ThumbUpIcon className="postClicked" />
+              <p>Polubiono</p>
+            </>
+          ) : (
+            <>
+              <ThumbUpIcon />
+              <p>Polub</p>
+            </>
+          )}
+        </div>
+
+        <div className="postOpt" onClick={handleCommentClick}>
+          <CommentIcon />
+          <p>Dodaj komentarz</p>
+        </div>
+      </div>
+
+      <div className="postComms">
+        {post &&
+          post.comments.length !== 0 &&
+          post.comments.map((comment, index) => {
+            return <Comment key={index} comment={comment} />;
+          })}
+      </div>
+
+      {isClickedComment ? (
+        <form className="addComment" onSubmit={handleCommentSubmit}>
+          <input
+            type="text"
+            placeholder="Treść komentarza..."
+            className="addCommentInput"
+            onChange={handleCommentChange}
+            value={commentContent}
+          />
+          <br />
+          <button type="submit" className="addCommentBtn">
+            Dodaj
+          </button>
+        </form>
+      ) : null}
+    </div>
+  );
 }
 
 export default Post;
